5a03c5565a30ddefc9fd6309be9a0b3d201cf967
mfaisalrayhan <airn01137@gmail.com>
2022-03-23 23:35:23 +0700

    download codeigniter

---
+++ pustakabook--no-dev/app/Controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Controllers;
+class Latihan1 extends BaseController
+{
+    public function index()
+    {
+        echo"Selamat Datang.. selamat belajar Web Programing";
+    }
+}
